70e036a6ae24d9f45c9f1d4d0fed1c350a82f06e
Dmitrii Metelkin <dmitriim@catalyst-au.net>
2017-12-19 11:29:24 +1100

    Add js for the player

---
+++ amd/src/brightcove.js
@@ -0,0 +1,28 @@
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @module local_brightcove/brightcove
+ */
+define(['bc'], function() {
+
+    var Brightcove = {};
+
+    Brightcove.init = function (playerid) {
+        // We can fire some functionality here and attach to the player.
+    };
+
+    return Brightcove;
+});
